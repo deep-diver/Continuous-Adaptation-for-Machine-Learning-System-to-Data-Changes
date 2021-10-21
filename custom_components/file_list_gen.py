3af1daa09e287d5152b483f8bfd672f505d80824
Chansung Park <deep.diver.csp@gmail.com>
2021-10-21 16:17:12 +0000

    refactor filelistgen component

--- custom_components/file_list_gen.py
+++ custom_components/file_list_gen.py
@@ -1,45 +1,46 @@
-import tfx
-from tfx.dsl.component.experimental.decorators import component
-from tfx.dsl.component.experimental.annotations import Parameter
-from tfx.dsl.component.experimental.annotations import OutputArtifact
-from tfx.types.standard_artifacts import String
-from google.cloud import storage
+import tensorflow as tf
 from absl import logging
+
+from tfx.dsl.component.experimental.decorators import component
+from tfx.dsl.component.experimental.annotations import Parameter, OutputArtifact
+from tfx.types.standard_artifacts import String
 
 
 @component
 def FileListGen(
     outpath: OutputArtifact[String],
-    project: Parameter[str],
-    gcs_source_bucket: Parameter[str],
-    gcs_source_prefix: Parameter[str] = "",
+    gcs_src_bucket: Parameter[str],
+    gcs_src_prefix: Parameter[str] = "",
     output_filename: Parameter[str] = "test-images.txt",
 ):
+    """
+    Generate a txt file formatted required by Vertex AI's Batch Prediction
+    There are few options, and this component generate "file list" formatted txt.
+    (https://cloud.google.com/vertex-ai/docs/predictions/batch-predictions)
+
+    outpath: OutputArtifact to hold where output_filename will be located
+             This will be used in the downstream component, BatchPredictionGen
+    gcs_src_bucket: GCS bucket name where the list of raw data is
+    gcs_src_prefix: prefix to be added to gcs_src_bucket
+    output_filename: output filename whose content is a list of file paths of raw data
+    """
     logging.info("FileListGen started")
 
-    client = storage.Client(project=project)
-    bucket = client.get_bucket(gcs_source_bucket)
-    blobs = bucket.list_blobs(prefix=gcs_source_prefix)
+    gcs_src_prefix = (
+        f"{gcs_src_prefix}/" if len(gcs_src_prefix) != 0 else gcs_src_prefix
+    )
+    img_paths = tf.io.gfile.glob(f"gs://{gcs_src_bucket}/{gcs_src_prefix}*.jpg")
     logging.info("Successfully retrieve the file(jpg) list from GCS path")
 
-    f = open(output_filename, "w")
-    for blob in blobs:
-        if blob.name.split(".")[-1] == "jpg":
-            prefix = ""
-            if gcs_source_prefix != "":
-                prefix = f"/{gcs_source_prefix}"
-            line = f"gs://{gcs_source_bucket}{prefix}/{blob.name}\n"
-            f.write(line)
-    f.close()
+    with open(output_filename, "w", encoding="utf-8") as f:
+        f.writelines("%s\n" % img_path for img_path in img_paths)
+
     logging.info(
         f"Successfully created the file list file({output_filename}) in local storage"
     )
 
-    prefix = ""
-    if gcs_source_prefix != "":
-        prefix = f"{gcs_source_prefix}/"
-    blob = bucket.blob(f"{prefix}{output_filename}")
-    blob.upload_from_filename(output_filename)
-    logging.info(f"Successfully uploaded the file list ({prefix}{output_filename})")
+    gcs_dst = f"gs://{gcs_src_bucket}/{gcs_src_prefix}{output_filename}"
+    tf.io.gfile.copy(output_filename, gcs_dst)
+    logging.info(f"Successfully uploaded the file list ({gcs_dst})")
 
-    outpath.value = gcs_source_bucket + "/" + prefix + output_filename
+    outpath.value = gcs_dst
